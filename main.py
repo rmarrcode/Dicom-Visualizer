e0c135d40ca7d47f17114dd8b2cb686950640232
marr0127 <marr0127@umn.edu>
2021-04-12 20:37:13 -0500

    more funcs

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ import plotly
 from plotly.graph_objs import *
 import chart_studio.plotly as py
 
-
 def load_scan(path):
+    #get all slices as pydicom array
     slices = [pydicom.dcmread(path + '/' + s) for s in               
               os.listdir(path)]
     slices = [s for s in slices if 'SliceLocation' in s]
     slices.sort(key = lambda x: int(x.InstanceNumber))
+    #finding slice_thickness from all slices
     try:
         slice_thickness = np.abs(slices[0].ImagePositionPatient[2] -   
                           slices[1].ImagePositionPatient[2])
@@ -32,12 +33,11 @@ def load_scan(path):
     return slices
     
 def get_pixels_hu(scans):
+    #getting pixel values
     image = np.stack([s.pixel_array for s in scans])
-    image = image.astype(np.int16)    # Set outside-of-scan pixels to 0
-    # The intercept is usually -1024, so air is approximately 0
+    image = image.astype(np.int16)    
     image[image == -2000] = 0
-
-    # Convert to Hounsfield units (HU)
+    #linear conversion pixels
     intercept = scans[0].RescaleIntercept
     slope = scans[0].RescaleSlope
 
@@ -50,9 +50,76 @@ def get_pixels_hu(scans):
     return np.array(image, dtype=np.int16)
 
 
-path =  '/home/ryan/Desktop/DCM/chest'
-patient_dicom = load_scan(path)
-patient_pixels = get_pixels_hu(patient_dicom)
 
-plt.imshow(patient_pixels[326], cmap=plt.cm.bone)
-plt.show()
+def largest_label_volume(im, bg=-1):
+    vals, counts = np.unique(im, return_counts=True)    
+    counts = counts[vals != bg]
+    vals = vals[vals != bg]    
+    if len(counts) > 0:
+        return vals[np.argmax(counts)]
+    else:
+        return None
+
+def segment_lung_mask(image, fill_lung_structures=True):
+    # not actually binary, but 1 and 2. 
+    # 0 is treated as background, which we do not want
+    binary_image = np.array(image >= -700, dtype=np.int8)+1
+    labels = measure.label(binary_image)
+ 
+    # Pick the pixel in the very corner to determine which label is air
+    # Improvement: Pick multiple background labels from around the patient 
+    # More resistant to “trays” on which the patient lays cutting the air around the person in half 
+    background_label = labels[0,0,0]
+ 
+    # Fill the air around the person
+    # air becomes 2 ?
+    binary_image[background_label == labels] = 2
+
+ 
+    # Method of filling the lung structures (that is superior to 
+    # something like morphological closing)
+    if fill_lung_structures:
+        # For every slice we determine the largest solid structure
+        for i, axial_slice in enumerate(binary_image):
+            axial_slice = axial_slice - 1
+            labeling = measure.label(axial_slice)
+            l_max = largest_label_volume(labeling, bg=0)
+
+            if l_max is not None: #This slice contains some lung
+                binary_image[i][labeling != l_max] = 1
+    binary_image -= 1 #Make the image actual binary
+    binary_image = 1-binary_image # Invert it, lungs are now 1
+ 
+    # Remove other air pockets inside body
+    labels = measure.label(binary_image, background=0)
+    l_max = largest_label_volume(labels, bg=0)
+    if l_max is not None: # There are air pockets
+        binary_image[labels != l_max] = 0
+ 
+    return binary_image
+
+def main():
+    path =  '/home/ryan/Desktop/DCM/chest'
+    patient_dicom = load_scan(path)
+    patient_pixels = get_pixels_hu(patient_dicom)
+
+    plt.imshow(patient_pixels[326], cmap=plt.cm.bone)
+    plt.show()
+
+    segmented_lungs = segment_lung_mask(patient_pixels, fill_lung_structures=False)    
+    segmented_lungs_fill = segment_lung_mask(patient_pixels, fill_lung_structures=True)
+        
+    internal_structures = segmented_lungs_fill - segmented_lungs
+    copied_pixels = copy.deepcopy(patient_pixels)
+    for i, mask in enumerate(segmented_lungs_fill): 
+        #find where not lungs are found
+        get_high_vals = mask == 0
+        copied_pixels[i][get_high_vals] = 0
+    seg_lung_pixels = copied_pixels
+
+    #plt.imshow(seg_lung_pixels[0], cmap=plt.cm.bone)
+    #plt.imshow(seg_lung_pixels[300], cmap=plt.cm.bone)
+    #plt.show()
+
+if __name__ == "__main__":
+    main()
