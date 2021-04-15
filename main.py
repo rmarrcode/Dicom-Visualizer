d21d3ea620ccba6466cfb1d7ef3095e38bbae284
marr0127 <marr0127@umn.edu>
2021-04-14 21:19:05 -0500

    GK

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ import ipywidgets as widgets
 import plotly
 from plotly.graph_objs import *
 import chart_studio.plotly as py
+from GK import * 
 
+#takes a pathname opens dicom files 
 def load_scan(path):
     #get all slices as pydicom array
     slices = [pydicom.dcmread(path + '/' + s) for s in               
@@ -32,8 +34,10 @@ def load_scan(path):
         s.SliceThickness = slice_thickness
     return slices
     
+#pixels -> hu
 def get_pixels_hu(scans):
     #getting pixel values
+    
     image = np.stack([s.pixel_array for s in scans])
     image = image.astype(np.int16)    
     image[image == -2000] = 0
@@ -49,8 +53,6 @@ def get_pixels_hu(scans):
 
     return np.array(image, dtype=np.int16)
 
-
-
 def largest_label_volume(im, bg=-1):
     vals, counts = np.unique(im, return_counts=True)    
     counts = counts[vals != bg]
@@ -60,6 +62,8 @@ def largest_label_volume(im, bg=-1):
     else:
         return None
 
+#produces a binary mask for a lung
+#change constant
 def segment_lung_mask(image, fill_lung_structures=True):
     # not actually binary, but 1 and 2. 
     # 0 is treated as background, which we do not want
@@ -103,8 +107,8 @@ def main():
     patient_dicom = load_scan(path)
     patient_pixels = get_pixels_hu(patient_dicom)
 
-    plt.imshow(patient_pixels[326], cmap=plt.cm.bone)
-    plt.show()
+    #plt.imshow(patient_pixels[326], cmap=plt.cm.bone)
+    #plt.show()
 
     segmented_lungs = segment_lung_mask(patient_pixels, fill_lung_structures=False)    
     segmented_lungs_fill = segment_lung_mask(patient_pixels, fill_lung_structures=True)
@@ -118,8 +122,12 @@ def main():
     seg_lung_pixels = copied_pixels
 
     #plt.imshow(seg_lung_pixels[0], cmap=plt.cm.bone)
-    #plt.imshow(seg_lung_pixels[300], cmap=plt.cm.bone)
+    #plt.imshow(seg_lung_pixels[15], cmap=plt.cm.bone)
     #plt.show()
+
+    selected_slices = seg_lung_pixels[0]
+    gk_clustered_imgs = np.array([gk_segment(x) for x in selected_slices])   
+    plt.imshow(gk_clustered_imgs[0], cmap=plt.cm.bone)
 
 if __name__ == "__main__":
     main()
